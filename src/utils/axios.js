11cdb418c69ab8d86b80fd6a1011a8fddc250759
Hải Trần <sl4mdunk1999@gmail.com>
2022-09-05 18:10:36 +0700

    Prod API

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,10 @@ import * as Axios from 'axios';
 import Cookies from 'universal-cookie';
 
 export const axios = Axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? 'http://54.204.72.174/api/v1'
+      : 'http://localhost:8080/api/v1',
   headers: { 'Content-type': 'application/json' },
 });
 
