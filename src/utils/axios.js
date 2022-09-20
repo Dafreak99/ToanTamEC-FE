7645cde2b6dae2aa98bae2ca1da1fcf3d6dd8b56
Hải Trần <sl4mdunk1999@gmail.com>
2022-09-20 20:48:06 +0700

    Implement Refresh Token

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,4 +1,5 @@
 import * as Axios from 'axios';
+import jwtDecode from 'jwt-decode';
 import Cookies from 'universal-cookie';
 
 export const axios = Axios.create({
@@ -7,19 +8,41 @@ export const axios = Axios.create({
       ? 'https://toantam.tk/api/v1'
       : 'http://localhost:8080/api/v1',
   headers: { 'Content-type': 'application/json' },
+  withCredentials: true,
 });
 
 axios.interceptors.request.use(
-  (config) => {
+  async (config) => {
     const cookies = new Cookies();
 
     console.log(`%cMaking request to /${config.url}`, 'color: #73a9ff');
 
-    if (config.url === '/' || config.url.indexOf('/refreshToken') >= 0) {
+    if (config.url === '/' || config.url.indexOf('/user/refresh-token') >= 0) {
       return config;
     }
 
-    const token = cookies.get('accessToken');
+    let token = cookies.get('accessToken');
+
+    if (token) {
+      const { exp } = jwtDecode(token);
+
+      if (exp * 1000 <= Date.now()) {
+        console.log('%cExpired! Perform slient refresh', 'color: #ffc852');
+
+        try {
+          const {
+            data: { accessToken },
+          } = await axios.post('/user/refresh-token');
+          cookies.set('accessToken', accessToken);
+          token = accessToken;
+        } catch (error) {
+          if (error.response.status === 401) {
+            cookies.remove('accessToken');
+            window.location.href = '/';
+          }
+        }
+      }
+    }
 
     return {
       ...config,
